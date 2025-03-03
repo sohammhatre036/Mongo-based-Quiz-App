119f934d74749f3538e4ff2725248af5e5f4471e
Soham Mhatre <sohammhatre036@gmail.com>
2025-03-03 18:51:11 +0530

    Update config.py
    conn uri

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "your_default_fallback_uri")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://quizapp18-server:Q9w0BslnOApUsjlitg5fWxyfBtqfCVDrTSoUPlCVQpz4tNIFSP4Za9N7mWZFXfHzxTQLiMrQr8jEACDbYBK2UQ==@quizapp18-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@quizapp18-server@")
